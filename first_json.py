3e3c434ae3ac998c0c5295a5f8486e8ab1c95dc5
yun0zi <yuki.imamura.biz@gmail.com>
2022-01-23 23:20:24 +0900

    modify description

--- first_json.py
+++ first_json.py
@@ -6,7 +6,7 @@ END_NUMBER = 100
 for number in range(START_NUMBER, END_NUMBER+1):
     print(number)
     json_data = {
-        "description": 'Baby Sharks Guild is a collection that includes 10,000 kinds of unique sharks. In the collection, you can definitely find your best poppy and kawaii here. Please see BabySharksGuild.com for the details. \n\nThis Studios is made up of unrecognized young creators in Japan. It was established thanks to the cooperation of entrepreneurs, engineers, interpreters, designers, illustrators, GiAf animators, video creators and planners. \n\n This amazing Gif animation was created by ”Y.Nagatsuka” in ordinary paper-drawing way.',
+        "description": 'Baby Sharks Guild is a collection that includes 10,000 kinds of unique sharks. In the collection, you can definitely find your best poppy and kawaii here. Please see [BabySharksGuild.com](https://babysharksguild.com/) for the details. \n\nThis Studios is made up of unrecognized young creators in Japan. It was established thanks to the cooperation of entrepreneurs, engineers, interpreters, designers, illustrators, GiAf animators, video creators and planners. \n\n This amazing Gif animation was created by **Y.Nagatsuka** in ordinary paper-drawing way.',
         "image": "https://metadata.babysharksguild.com/images/0.gif",
         "name": f"Baby Shark #{number}",
         "attributes": [{
