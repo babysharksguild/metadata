649e7151d322a858e8b0fb1e169d91c796482d49
yun0zi <yuki.imamura.biz@gmail.com>
2022-01-23 22:11:57 +0900

    add test metadata

--- secretjson.py
+++ secretjson.py
@@ -6,7 +6,7 @@ END_NUMBER = 10000
 for number in range(START_NUMBER, END_NUMBER+1):
     print(number)
     json_data = {
-        "description": "Baby Sharks Guild",
+        "description": "Baby Sharks Guild, ",
         "image": "https://metadata.babysharksguild.com/images/0.gif",
         "name": f"Baby Shark #{number}",
         "attributes": [{
